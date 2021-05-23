980d7862a4e3acdd7a58db24288a040245235103
Brandon <brandon.eg@gmail.com>
2021-05-23 01:09:23 -0700

    added images to portfolio page

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,34 +1,40 @@
 import React from 'react';
+import averageJoeImage from '../../assets/portfolio-images/average-joe.PNG';
+import mailFlyerImage from '../../assets/portfolio-images/mailflyer.png';
+import bookSearchImage from '../../assets/portfolio-images/book-search.png';
+import safePassageImage from '../../assets/portfolio-images/safe-passage.PNG';
+import horiseonImage from '../../assets/portfolio-images/horiseon.png';
+import runBuddyImage from '../../assets/portfolio-images/run-buddy.png';
 
 const projects = [
   {
     link: "https://github.com/patrickmklee/AverageJoeFitness",
-    img: "",
+    img: averageJoeImage,
     alt: "Average Joe's Fitness",
   },
   {
     link: "https://mailflyer.herokuapp.com/",
-    img: "",
+    img: mailFlyerImage,
     alt: "Mail Flyer",
   },
   {
     link: "https://boiling-journey-97325.herokuapp.com/",
-    img: "",
+    img: bookSearchImage,
     alt: "Google Book Search",
   },
   {
     link: "https://jaderiver62.github.io/safe-passage/",
-    img: "",
+    img: safePassageImage,
     alt: "Safe Passage",
   },
   {
     link: "https://brandonag.github.io/horiseon/",
-    img: "",
+    img: horiseonImage,
     alt: "Horiseon",
   },
   {
     link: "https://brandonag.github.io/run-buddy/",
-    img: "",
+    img: runBuddyImage,
     alt: "Run Buddy",
   },
 ]
@@ -43,7 +49,7 @@ function Portfolio() {
           <div class="col s12 m6 l4">           
             <div className="card">
               <div className="card-image waves-effect waves-block waves-light">
-                <a href={project.link}>
+                <a href={project.link} target="_blank">
                   <img width="305" height="229" src={project.img} class="responsive-img" alt={project.name} loading="lazy" title={project.name} />
                   <span class="card-title">{project.name}</span>
                 </a>
@@ -51,17 +57,6 @@ function Portfolio() {
             </div>
           </div>
         ))}
-
-        {/* <div class="col s12 m6 l4">           
-          <div className="card">
-            <div className="card-image waves-effect waves-block waves-light">
-              <a href="https://adbeus.com/montreal/st-henri/buck-15-saint-henri-montreal/">
-                <img width="305" height="229" src="https://adbeus.com/wp-content/uploads/2016/11/rlbmlm0nc3qlivffw35l-305x229.jpg" class="responsive-img" alt="Café Parvis / Downtown, Montreal" loading="lazy" title="Café Parvis / Downtown, Montreal" />
-                <span class="card-title">Buck 15</span>
-              </a>
-            </div>
-          </div>
-        </div> */}
       </div>
     </section>
   );
