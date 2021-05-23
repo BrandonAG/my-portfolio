b7154e217452a8c35402a9f80b2cee097bb545ba
Brandon <brandon.eg@gmail.com>
2021-05-23 01:12:21 -0700

    Updates

---
+++ precache-manifest.c342c4d76f30e155e6de1e30936f210f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a664d1730b8176f0b7c180311673fcd",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "09fc90b9065953547629",
+    "url": "/my-portfolio/static/css/main.0e296bbb.chunk.css"
+  },
+  {
+    "revision": "751fe094b98f1ed26572",
+    "url": "/my-portfolio/static/js/2.2d038866.chunk.js"
+  },
+  {
+    "revision": "cfcd6bb9d3632198657976524f1b10e2",
+    "url": "/my-portfolio/static/js/2.2d038866.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09fc90b9065953547629",
+    "url": "/my-portfolio/static/js/main.13a547eb.chunk.js"
+  },
+  {
+    "revision": "5d4511f3fcec99c33304",
+    "url": "/my-portfolio/static/js/runtime-main.c58b4907.js"
+  },
+  {
+    "revision": "d886813b1e623c1674e24509839c39a2",
+    "url": "/my-portfolio/static/media/average-joe.d886813b.PNG"
+  },
+  {
+    "revision": "009768acea334921a1a226b4d571a713",
+    "url": "/my-portfolio/static/media/book-search.009768ac.png"
+  },
+  {
+    "revision": "8cfdf18a3aadd9a461d8b13734e251f5",
+    "url": "/my-portfolio/static/media/horiseon.8cfdf18a.png"
+  },
+  {
+    "revision": "6efa54770608fa3f4abccd00b65ca06a",
+    "url": "/my-portfolio/static/media/mailflyer.6efa5477.png"
+  },
+  {
+    "revision": "0c54ceacd379f14999b71349e519d298",
+    "url": "/my-portfolio/static/media/portrait-brandon.0c54ceac.jpg"
+  },
+  {
+    "revision": "e52807f6472f1f8c6f551865a7ea35b1",
+    "url": "/my-portfolio/static/media/run-buddy.e52807f6.png"
+  },
+  {
+    "revision": "3c68560d671579b45983aa14852b5a43",
+    "url": "/my-portfolio/static/media/safe-passage.3c68560d.PNG"
+  }
+]);
